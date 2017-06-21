05e5d37285911fe2f8ef0f18b9f605fe8c68b4d9
davidfeng88 <david.feng.ge@gmail.com>
2017-06-21 18:02:07 -0400

    darken homehero background

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
+User.destroy_all
 User.create(username: 'guest', password: 'password')
