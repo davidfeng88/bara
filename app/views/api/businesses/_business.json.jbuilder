15091bd50bcaa6c82f3f23994fbff8d6f0f0c38e
davidfeng88 <david.feng.ge@gmail.com>
2017-06-25 16:44:47 -0400

    Style business index item

--- app/views/api/businesses/_business.json.jbuilder
+++ app/views/api/businesses/_business.json.jbuilder
@@ -1,2 +1,3 @@
-json.extract! business, :id, :author_id, :name, :address, :city, :state, :zipcode, :price
+json.extract! business,
+  :id, :author_id, :name, :address, :city, :state, :zipcode, :price, :url, :phone
 json.image_url asset_path(business.image.url)
--- frontend/components/business/business_index_item.jsx
+++ frontend/components/business/business_index_item.jsx
@@ -3,13 +3,38 @@ import { Link } from 'react-router-dom';
 
 const BusinessIndexItem = ({ business }) => {
   return(
-    <li>
-      <Link to={`/businesses/${business.id}`}>{business.name}</Link>
-      <img src={business.image_url} />
-      <p>{business.address}</p>
-      <p>{`${business.city}, ${business.state} ${business.zipcode}`}</p>
-      <p>{business.phone}</p>
-    </li>
+    <div className='index-item'>
+
+      <div className='index-item-row1'>
+
+        <div className='index-pic'>
+          <img src={business.image_url} />
+        </div>
+
+        <div className='index-li'>
+          <div className='col1'>
+            <li>
+              <Link to={`/businesses/${business.id}`}>{business.name}</Link>
+            </li>
+            Reviews Info goes here<br/>
+            Price: {business.price}<br/>
+            Business Tags<br/>
+          </div>
+
+          <div className='col2'>
+            {business.address}<br/>
+            {`${business.city}, ${business.state} ${business.zipcode}`}<br/>
+            {business.phone}<br/>
+          </div>
+
+        </div>
+
+      </div>
+
+      <div className='index-item-row2'>
+        Top review for this business goes here
+      </div>
+    </div>
   );
 };
 
