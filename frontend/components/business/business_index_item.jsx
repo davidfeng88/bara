e018bf2c3f15b64d4186eda896989db1f9d9414f
davidfeng88 <david.feng.ge@gmail.com>
2017-06-25 16:57:23 -0400

    convert price to $

--- frontend/components/business/business_index_item.jsx
+++ frontend/components/business/business_index_item.jsx
@@ -1,41 +1,61 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const BusinessIndexItem = ({ business }) => {
-  return(
-    <div className='index-item'>
+class BusinessIndexItem extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
-      <div className='index-item-row1'>
+  price(number) {
+    switch (number) {
+      case 1:
+        return '$';
+      case 2:
+        return '$$';
+      case 3:
+        return '$$$';
+      case 4:
+        return '$$$$';
+    }
+  }
 
-        <div className='index-pic'>
-          <img src={business.image_url} />
-        </div>
+  render() {
+    const { business } = this.props;
+    return (
+      <div className='index-item'>
 
-        <div className='index-li'>
-          <div className='col1'>
-            <li>
-              <Link to={`/businesses/${business.id}`}>{business.name}</Link>
-            </li>
-            Reviews Info goes here<br/>
-            Price: {business.price}<br/>
-            Business Tags<br/>
+        <div className='index-item-row1'>
+
+          <div className='index-pic'>
+            <img src={business.image_url} />
           </div>
 
-          <div className='col2'>
-            {business.address}<br/>
-            {`${business.city}, ${business.state} ${business.zipcode}`}<br/>
-            {business.phone}<br/>
+          <div className='index-li'>
+            <div className='col1'>
+              <li>
+                <Link to={`/businesses/${business.id}`}>{business.name}</Link>
+              </li>
+              Reviews Info goes here<br/>
+              {this.price(business.price)}  •  Business Tags<br/>
+            </div>
+
+            <div className='col2'>
+              {business.address}<br/>
+              {`${business.city}, ${business.state} ${business.zipcode}`}<br/>
+              {business.phone}<br/>
+            </div>
+
           </div>
 
         </div>
 
+        <div className='index-item-row2'>
+          Top review for this business goes here
+        </div>
       </div>
+    );
+  }
 
-      <div className='index-item-row2'>
-        Top review for this business goes here
-      </div>
-    </div>
-  );
-};
+}
 
 export default BusinessIndexItem;
