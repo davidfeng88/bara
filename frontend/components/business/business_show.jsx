c0e58cb0caa7a4cf2b3616314dd94c52a9e0a904
davidfeng88 <david.feng.ge@gmail.com>
2017-06-25 20:52:49 -0400

    style title row of business show page

--- frontend/components/business/business_show.jsx
+++ frontend/components/business/business_show.jsx
@@ -1,17 +1,78 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const BusinessShow = ({ business }) => {
-  return(
-    <div>
+class BusinessShow extends React.Component {
+  constructor(props) {
+    super(props);
 
-      <Link to={`/businesses/${business.id}`}>{business.name}</Link>
-      <p>{business.address}</p>
-      <p>{`${business.city}, ${business.state} ${business.zipcode}`}</p>
-      <p>{business.phone}</p>
-      <Link to='/businesses'> back to all businesses </Link>
-    </div>
-  );
-};
+
+  }
+
+  price(number) {
+    switch (number) {
+      case 1:
+        return '$';
+      case 2:
+        return '$$';
+      case 3:
+        return '$$$';
+      case 4:
+        return '$$$$';
+    }
+  }
+
+  render() {
+    const { business } = this.props;
+    return(
+      <div>
+        <div className='business-show-title'>
+          <div className='center'>
+            <h1>{business.name}</h1>
+            <div className='business-show-title-row1'>
+
+
+              <div className='business-show-title-col'>
+                Reviews Info goes here<br/>
+                {this.price(business.price)}  •  Business Tags<br/>
+              </div>
+
+              <div className='business-show-title-col'>
+                Write a review button<br/>
+              </div>
+            </div>
+
+            <div className='business-show-title-row2'>
+              <div className='info'>
+                <div className='show-map'>
+                  the static show map goes here
+                </div>
+                <div className='text-info'>
+                  {business.address}<br/>
+                  {`${business.city}, ${business.state} ${business.zipcode}`}<br/>
+                  {business.phone}<br/>
+                  {business.url}<br/>
+                  Edit Business Link here
+                </div>
+              </div>
+
+              <div className='pictures'>
+                <img src={business.image_url} />
+                <img src={business.image_url} />
+                <img src={business.image_url} />
+              </div>
+            </div>
+          </div>
+
+        </div>
+
+
+
+
+      </div>
+    );
+
+  }
+}
+
 
 export default BusinessShow;
