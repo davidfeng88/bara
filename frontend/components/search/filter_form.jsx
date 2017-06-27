16f0b98bf4d67ef6fd5d746e40ff5003a8b9af90
davidfeng88 <david.feng.ge@gmail.com>
2017-06-27 15:53:31 -0400

    style price filter

--- frontend/components/search/filter_form.jsx
+++ frontend/components/search/filter_form.jsx
@@ -5,23 +5,48 @@ const handleChange = (filter, updateFilter) => e => (
 );
 
 const FilterForm = ({ minPrice, maxPrice, updateFilter }) => (
-  <div>
-    <span className="filter">Filter results:</span>
+  <div className="filter">
+    <div>Filter results:</div>
     <br/>
-    <label>Minimum Price</label>
-    <input
-      type="number"
-      value={minPrice}
-      onChange={handleChange('minPrice', updateFilter)}
-    />
-     <br/>
-    <label>Maximum Price</label>
-    <input
-      type="number"
-      value={maxPrice}
-      onChange={handleChange('maxPrice', updateFilter)}
-    />
+
+    <div className='price-filter'>
+      <div className='price-filter-col'>
+        <label htmlFor='min-price'>Minimum Price</label>
+        <select id="min-price" value={minPrice}
+          onChange={handleChange('minPrice', updateFilter)} >
+          <option value='1' >$ - Inexpensive</option>
+          <option value='2' >$$ - Moderate</option>
+          <option value='3' >$$$ - Pricey</option>
+          <option value='4' >$$$$ - Ultra High-End</option>
+        </select>
+      </div>
+      <div className='price-filter-col'>
+        <label htmlFor='max-price'>Maximum Price</label>
+        <select id="max-price" value={maxPrice}
+          onChange={handleChange('maxPrice', updateFilter)} >
+          <option value='1' >$ - Inexpensive</option>
+          <option value='2' >$$ - Moderate</option>
+          <option value='3' >$$$ - Pricey</option>
+          <option value='4' >$$$$ - Ultra High-End</option>
+        </select>
+      </div>
+    </div>
+
   </div>
 );
 
 export default FilterForm;
+
+// <label>Minimum Price</label>
+// <input
+//   type="number"
+//   value={minPrice}
+//   onChange={handleChange('minPrice', updateFilter)}
+// />
+//
+// <label>Maximum Price</label>
+// <input
+//   type="number"
+//   value={maxPrice}
+//   onChange={handleChange('maxPrice', updateFilter)}
+// />
