d8610e027d836aae8a548f5923ad2bafe950add0
davidfeng88 <david.feng.ge@gmail.com>
2017-06-27 11:30:30 -0400

    remove home content

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -3,8 +3,6 @@ import { Route, Switch } from 'react-router-dom';
 
 import HeaderRouter from './header/header_router';
 
-import Home from './home/home';
-
 // bodies
 
 import SessionFormContainer from './session/session_form_container';
@@ -16,51 +14,32 @@ import Footer from './footer';
 
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
+const App = () => {
+  return (
+    <div  className='page-container'>
+      <HeaderRouter />
 
+      <div className='body'>
+      <i class="fa fa-cart-arrow-down" aria-hidden="true"></i>
 
-  }
-
-  // componentWillReceiveProps(newProps) {
-  //   if ( this.props.poke && this.props.poke.id !== parseInt(newProps.match.params.pokemonId) ) {
-  //     // THIS IS GETTING CALLED TWICE FOR NO REASON!!
-  //     // console.log("REQUESTING POKEMON");
-  //     this.props.requestOnePokemon(newProps.match.params.pokemonId);
-  //   }
-  // }
-
-
-  render() {
-    return (
-      <div  className='page-container'>
-        <HeaderRouter />
-
-        <div className='body'>
-        <i class="fa fa-cart-arrow-down" aria-hidden="true"></i>
-          <Route exact path='/' component={Home} />
-
-          <Switch>
-            <ProtectedRoute path="/businesses/new" component={BusinessForm} />
-            <ProtectedRoute path="/businesses/:id/edit"
-              component={BusinessForm} />
-            <Route path='/businesses/:id' component={BusinessShowContainer} />
-            <Route path='/businesses' component={BusinessIndexContainer} />
-          </Switch>
+        <Switch>
+          <ProtectedRoute path="/businesses/new" component={BusinessForm} />
+          <ProtectedRoute path="/businesses/:id/edit"
+            component={BusinessForm} />
+          <Route path='/businesses/:id' component={BusinessShowContainer} />
+          <Route path='/businesses' component={BusinessIndexContainer} />
+        </Switch>
 
 
 
-          <Switch>
-            <AuthRoute path="/login" component={SessionFormContainer} />
-            <AuthRoute path="/signup" component={SessionFormContainer} />
-          </Switch>
-        </div>
-        <Footer />
+        <Switch>
+          <AuthRoute path="/login" component={SessionFormContainer} />
+          <AuthRoute path="/signup" component={SessionFormContainer} />
+        </Switch>
       </div>
-    );
-  }
-
-}
+      <Footer />
+    </div>
+  );
+};
 
 export default App;
