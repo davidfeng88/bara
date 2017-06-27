bed2f14c7b6524ebf996b13a4b7c3e7ce661663a
davidfeng88 <david.feng.ge@gmail.com>
2017-06-27 11:05:01 -0400

    refactor home bar

--- frontend/components/header/home_bar.jsx
+++ frontend/components/header/home_bar.jsx
@@ -1,89 +1,72 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-      // <button >Demo Login</button>
-            // <div className='home-login'><Link to="/login">Log In</Link></div>
-const sessionLinks = (demoLogin) => (
-  <nav className="home-bar">
-    <div onClick={demoLogin} className='home-demo'>
-      Demo Login
-    </div>
-
-    <div className='home-bar-right'>
-      <div className='home-login'><Link to="/login">Log In</Link></div>
-      <div className='home-signup'><Link to="/signup">Sign up</Link></div>
-    </div>
-
-  </nav>
-);
-
-const personalGreeting = (dropdown, toggleDropdown, currentUser, logout) => (
-  <nav className='home-bar'>
-    <div className='home-bar-left'>
-    </div>
-    <div className='home-bar-right'>
-      <div onClick={toggleDropdown} className='dropdown' >
-        <img className='home-avatar' src={currentUser.avatar_url} />
-        <img className='down-icon' src={window.staticImages.downIcon} />
-      </div>
-      {dropdown(currentUser, logout)}
-    </div>
-
-  </nav>
-
-);
+import Dropdown from './dropdown';
 
 class HomeBar extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { showDropdown: false };
-
-    this.toggleDropdown = this.toggleDropdown.bind(this);
   }
 
-  toggleDropdown(e) {
-    e.preventDefault();
-    // if (e.currentTarget !=) {
-  //   this.setState = {showDropdown: false }
-  // } else {
-  //
-  // }
+  personalGreeting() {
+    let {
+      currentUser,
+      toggleDropdown,
+      demoLogin,
+      logout,
+      showDropdown
+    } = this.props;
+    let dropdownBox;
 
-    this.setState({showDropdown: !this.state.showDropdown });
-  }
+    if (showDropdown) {
+      dropdownBox =
+      <Dropdown
+        currentUser={currentUser}
+        logout={logout}
+        toggleDropdown={toggleDropdown} />;
+    } else {
+      dropdownBox = null;
+    }
 
-  dropdown(currentUser, logout) {
-    // debugger;
-    // if (this.state.showDropdown) {
-      return(
-        <div className='dropdown-box'>
-          <div className='dropdown-box-row1' >
-            <div className='dropdown-avatar-wrapper'>
-              <img className='dropdown-avatar' src={currentUser.avatar_url} />
-            </div>
-            <div className='dropdown-name'>{currentUser.username}</div>
-          </div>
-
-          <div onClick={logout} className='dropdown-box-row2' >
-            Log Out
-          </div>
+    return(
+      <nav className='home-bar'>
+        <div className='home-bar-left'>
         </div>
-      );
-    // } else {
-    //   return null;
-    // }
+        <div className='home-bar-right'>
+          <div onClick={toggleDropdown} className='dropdown' >
+            <img className='home-avatar' src={currentUser.avatar_url} />
+            <img className='down-icon' src={window.staticImages.downIcon} />
+          </div>
+          {dropdownBox}
+        </div>
+      </nav>
+    );
+  }
+
+  sessionLinks() {
+    return(
+      <nav className="home-bar">
+        <div onClick={this.props.demoLogin} className='home-demo'>
+          Demo Login
+        </div>
+
+        <div className='home-bar-right'>
+          <div className='home-login'><Link to="/login">Log In</Link></div>
+          <div className='home-signup'><Link to="/signup">Sign up</Link></div>
+        </div>
+
+      </nav>
+    );
   }
 
   render() {
-    let { currentUser, demoLogin, logout } = this.props;
-    if (currentUser) {
-      return personalGreeting(this.dropdown,
-        this.toggleDropdown, currentUser, logout);
+    if (this.props.currentUser) {
+      return this.personalGreeting();
     } else {
-      return sessionLinks(demoLogin);
+      return this.sessionLinks();
     }
   }
-}
 
+}
 
 export default HomeBar;
--- frontend/components/header/home_bar_container.js
+++ frontend/components/header/home_bar_container.js
@@ -1,16 +1,19 @@
 import { connect } from 'react-redux';
 
 import { login, logout } from '../../actions/session_actions';
+import { toggleDropdown } from '../../actions/dropdown_actions';
 
 import HomeBar from './home_bar';
 
-const mapStateToProps = ({ currentUser }) => ({
-  currentUser
+const mapStateToProps = ({ currentUser, showDropdown }) => ({
+  currentUser,
+  showDropdown
 });
 
 const mapDispatchToProps = dispatch => ({
   logout: () => dispatch(logout()),
-  demoLogin: () => dispatch(login({username: 'guest', password: 'password'}))
+  demoLogin: () => dispatch(login({username: 'guest', password: 'password'})),
+  toggleDropdown: () => dispatch(toggleDropdown())
 });
 
 export default connect(
