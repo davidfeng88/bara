327b37b5ce31d62fd53595a00d56b595b9d30417
davidfeng88 <david.feng.ge@gmail.com>
2017-06-27 11:15:37 -0400

    position the dropdown box

--- frontend/components/header/dropdown.jsx
+++ frontend/components/header/dropdown.jsx
@@ -5,16 +5,18 @@ const Dropdown = ({ currentUser, logout, toggleDropdown }) => {
   // toggle
   return (
     <div onClick={toggleDropdown} className="dropdown-background">
-      <div onClick={ e => e.stopPropagation() } className='dropdown-box'>
-        <div className='dropdown-box-row1' >
-          <div className='dropdown-avatar-wrapper'>
-            <img className='dropdown-avatar' src={currentUser.avatar_url} />
+      <div className='dropdown-container'>
+        <div onClick={ e => e.stopPropagation() } className='dropdown-box'>
+          <div className='dropdown-box-row1' >
+            <div className='dropdown-avatar-wrapper'>
+              <img className='dropdown-avatar' src={currentUser.avatar_url} />
+            </div>
+            <div className='dropdown-name'>{currentUser.username}</div>
           </div>
-          <div className='dropdown-name'>{currentUser.username}</div>
-        </div>
 
-        <div onClick={logout} className='dropdown-box-row2' >
-          Log Out
+          <div onClick={logout} className='dropdown-box-row2' >
+            Log Out
+          </div>
         </div>
       </div>
     </div>
