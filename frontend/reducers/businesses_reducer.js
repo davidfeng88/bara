54f08f8b161ae07b7b544ecd50b864861b177d93
davidfeng88 <david.feng.ge@gmail.com>
2017-06-28 16:30:30 -0400

    removed entities layer from business reducer and selectors, revise state shape

--- frontend/reducers/businesses_reducer.js
+++ frontend/reducers/businesses_reducer.js
@@ -8,10 +8,7 @@ import {
   REMOVE_REVIEW
 } from '../actions/business_actions.js';
 
-const defaultState = {
-  entities: {},
-  currentBusiness: -1
-};
+const defaultState = {};
 
 const BusinessesReducer = (state = defaultState, action) => {
   Object.freeze(state);
@@ -19,41 +16,40 @@ const BusinessesReducer = (state = defaultState, action) => {
 
   switch(action.type) {
     case RECEIVE_ALL_BUSINESSES:
-      return {entities: action.businesses, currentBusiness: -1};
+      return action.businesses;
 
     case RECEIVE_BUSINESS:
     // action.business.reviews is an array. Extract the id information
-      newState.entities[action.business.id] = action.business;
-      newState.currentBusiness = action.business.id;
-      const reviewIDs = action.business.reviews.map( review => review.id);
-      newState.entities[action.business.id].reviews = reviewIDs;
+      newState[action.business.id] = action.business;
+      const reviewIDs = action.business.reviews.map(review => review.id);
+      newState[action.business.id].reviews = reviewIDs;
       return newState;
 
     case REMOVE_BUSINESS:
-      delete newState.entities[action.business.id];
+      delete newState[action.business.id];
       return newState;
 
-    case RECEIVE_REVIEW:
-    // check if this is an edit or a create action.
-    // if edit, move the latest one to the top.
-      let array1 = newState.entities[newState.currentBusiness].reviews;
-      let id1 = action.review.id;
-      let idx1 = array1.indexOf(id);
-      if (idx1 > -1) {
-        array1.splice(idx1, 1);
-      }
-      newState.entities[newState.currentBusiness].reviews
-        .push(action.review.id);
-      return newState;
-
-    case REMOVE_REVIEW:
-      let array = newState.entities[newState.currentBusiness].reviews;
-      let id = action.review.id;
-      let idx = array.indexOf(id);
-      if (idx > -1) {
-        array.splice(idx, 1);
-      }
-      return newState;
+    // case RECEIVE_REVIEW:
+    // // check if this is an edit or a create action.
+    // // if edit, move the latest one to the top.
+    //   let array1 = newState.entities[newState.currentBusiness].reviews;
+    //   let id1 = action.review.id;
+    //   let idx1 = array1.indexOf(id);
+    //   if (idx1 > -1) {
+    //     array1.splice(idx1, 1);
+    //   }
+    //   newState.entities[newState.currentBusiness].reviews
+    //     .push(action.review.id);
+    //   return newState;
+    //
+    // case REMOVE_REVIEW:
+    //   let array = newState.entities[newState.currentBusiness].reviews;
+    //   let id = action.review.id;
+    //   let idx = array.indexOf(id);
+    //   if (idx > -1) {
+    //     array.splice(idx, 1);
+    //   }
+    //   return newState;
 
     default:
       return state;
@@ -64,35 +60,29 @@ export default BusinessesReducer;
 
 // Sample State Shape
 // {
-//   entities: {
+//   1: {
+//     id: 1;
+//     name: "Good Pizza",
+//     author_id: 1,
+//     address 1: "555 W. 25th St.",
+//     city: "Chicago",
+//     state: "IL",
+//     zip: 60637,
+//     phone: "(111)111-1111",
+//     price: 1,
+//     reviews: [1, 2]
+//     },
 //
-//     1: {
-//       id: 1;
-//       name: "Good Pizza",
-//       author_id: 1,
-//       address 1: "555 W. 25th St.",
-//       city: "Chicago",
-//       state: "IL",
-//       zip: 60637,
-//       phone: "(111)111-1111",
-//       price: 1,
-//       reviews: [1, 2]
-//       },
-//
-//     2: {
-//       id: 2;
-//       name: "Great Pizza",
-//       author_id: 1,
-//       address 1: "666 W. 25th St.",
-//       city: "Chicago",
-//       state: "IL",
-//       zip: 60637,
-//       phone: "(222)222-2222",
-//       price: 2,
-//       reviews: [3]
-//     }
-//   },
-//
-//   currentBusiness: 1
-//
+//   2: {
+//     id: 2;
+//     name: "Great Pizza",
+//     author_id: 1,
+//     address 1: "666 W. 25th St.",
+//     city: "Chicago",
+//     state: "IL",
+//     zip: 60637,
+//     phone: "(222)222-2222",
+//     price: 2,
+//     reviews: [3]
+//   }
 // }
--- frontend/reducers/selectors.js
+++ frontend/reducers/selectors.js
@@ -1,12 +1,12 @@
 export const businessesToArray = state => {
   let businessesArray = [];
-  businessesArray = Object.keys(state.businesses.entities)
-    .map(id => state.businesses.entities[id]);
+  businessesArray = Object.keys(state.businesses)
+    .map(id => state.businesses[id]);
   return businessesArray;
 };
 
 export const selectCurrentBusiness = (state, businessId) => {
-  return state.businesses.entities[businessId];
+  return state.businesses[businessId];
 };
 
 export const reviewsToArray = (state, business) => {
