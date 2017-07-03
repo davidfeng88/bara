8e45e0ead9123559a6beebdf8181362cec75ccc2
davidfeng88 <david.feng.ge@gmail.com>
2017-07-03 14:29:48 -0400

    get Bara production ready

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,14 +1,21 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import logger from 'redux-logger';
 
 import RootReducer from '../reducers/root_reducer';
+
+const middlewares = [thunk];
+
+if (process.env.NODE_ENV !== 'production') {
+  // must use 'require' (import only allowed at top of file)
+  const { createLogger } = require('redux-logger');
+  middlewares.push(createLogger());
+}
 
 const configureStore = (preloadedState = {}) => (
   createStore(
     RootReducer,
     preloadedState,
-    applyMiddleware(thunk, logger)
+    applyMiddleware(...middlewares)
   )
 );
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,27 @@
 const path = require("path");
 
+const webpack = require("webpack");
+
+let plugins = []; // if using any plugins for both dev and production
+const devPlugins = []; // if using any plugins for development
+
+const prodPlugins = [
+  new webpack.DefinePlugin({
+    'process.env': {
+      'NODE_ENV': JSON.stringify('production')
+    }
+  }),
+  new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: true
+    }
+  })
+];
+
+plugins = plugins.concat(
+  process.env.NODE_ENV === 'production' ? prodPlugins : devPlugins
+);
+
 module.exports = {
   context: __dirname,
   entry: "./frontend/bara.jsx",
@@ -7,6 +29,7 @@ module.exports = {
     path: path.join(__dirname, 'app', 'assets', 'javascripts'),
     filename: "bundle.js"
   },
+  plugins: plugins,
   resolve: {
     extensions: ["*", ".js", ".jsx"]
   },
