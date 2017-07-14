4fc6d85cfb7266b96f944425ffd71ac3c81ed640
davidfeng88 <david.feng.ge@gmail.com>
2017-07-14 16:45:50 -0400

    try to add a second home background

--- frontend/components/header/home_header.jsx
+++ frontend/components/header/home_header.jsx
@@ -5,21 +5,43 @@ import HomeBarContainer from './home_bar_container';
 import SearchBar from './search_bar';
 import HomeLinks from './home_links';
 
-const HomeHeader = () => {
-  return(
-    <div className='home-header'>
-      <HomeBarContainer />
-      <div className='logo'>
-        <Link to="/">
-          <img src={window.staticImages.homeLogo} />
-        </Link>
+class HomeHeader extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(e) {
+    e.preventDefault();
+    if (this.className === 'home-header') {
+      this.className = 'home-header2';
+    }
+
+  }
+
+
+
+
+  render() {
+    let style = {
+      backgroundImage: `asset-url("home.jpeg")`,
+    };
+    return(
+      <div style={style} className='home-header'>
+        <HomeBarContainer />
+        <div className='logo' onClick={this.handleClick}>
+          <Link to="/">
+            <img src={window.staticImages.homeLogo} />
+          </Link>
+        </div>
+        <div className='home-search'>
+          <SearchBar />
+        </div>
+        <HomeLinks />
       </div>
-      <div className='home-search'>
-        <SearchBar />
-      </div>
-      <HomeLinks />
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default HomeHeader;
