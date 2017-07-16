1f0fafa75a16d7840b74bd2739aa311358ccd627
davidfeng88 <david.feng.ge@gmail.com>
2017-07-16 10:06:01 -0400

    stick map by css

--- frontend/components/search/business_index.jsx
+++ frontend/components/search/business_index.jsx
@@ -4,7 +4,6 @@ import { Link } from 'react-router-dom';
 import BusinessIndexItem from './business_index_item';
 import IndexMap from './index_map';
 
-
 class BusinessIndex extends React.Component {
 
   render() {
@@ -18,28 +17,28 @@ class BusinessIndex extends React.Component {
       );
     }
     return(
-      <div>
-        <div className='business-index-main'>
-          <div className='center index-grid'>
-            <div className='index-grid-col1'>
-              <ol className="business-index">
-                { businessesEntries }
-              </ol>
-            </div>
-
-            <div className='index-grid-col2'>
-              <IndexMap businesses={this.props.businesses} />
-              <div className='add-business'>
-                <h3>Not here? Tell us what we're missing.</h3>
-                <p>If the business you're looking for isn't here, add it!</p>
-                <div className='add-business-link'>
-                  <Link to='/businesses/new'>Add A Business</Link>
-                </div>
-              </div>
-
+      <div className='business-index-main'>
+        <div className='center index-grid-row1'>
+          <div className='index-grid-col1'>
+            <ol className="business-index">
+              { businessesEntries }
+            </ol>
+          </div>
+          <div className='index-grid-col2'>
+            <div className='css-sticky'>
+             <IndexMap businesses={this.props.businesses} />
             </div>
           </div>
+        </div>
 
+        <div className='center index-grid-row2'>
+          <div className='add-business'>
+            <h3>Not here? Tell us what we're missing.</h3>
+            <p>If the business you're looking for isn't here, add it!</p>
+            <div className='add-business-link'>
+              <Link to='/businesses/new'>Add A Business</Link>
+            </div>
+          </div>
         </div>
       </div>
     );
