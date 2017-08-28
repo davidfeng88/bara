80aff335fce4003f965740a6e04a311644fd84e1
davidfeng88 <david.feng.ge@gmail.com>
2017-08-28 19:32:44 -0400

    refactor homebar

--- frontend/components/header/home_bar.jsx
+++ frontend/components/header/home_bar.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
 import Dropdown from './dropdown';
 
-class HomeBar extends React.Component {
+export default class HomeBar extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -27,7 +26,7 @@ class HomeBar extends React.Component {
     this.props.logout();
   }
 
-  personalGreeting() {
+  render() {
     let {
       currentUser,
       demoLogin,
@@ -42,44 +41,32 @@ class HomeBar extends React.Component {
         toggleDropdown={this.toggleDropdown} />;
     }
 
-    return(
-      <nav className='home-bar'>
-        <div className='home-bar-left'>
-        </div>
-        <div className='home-bar-right'>
-          <div onClick={this.toggleDropdown} className='dropdown' >
-            <img className='home-avatar' src={currentUser.avatar_url} />
-            <img className='down-icon' src={window.staticImages.downIcon} />
+    if (currentUser) {
+      return(
+        <nav className='home-bar'>
+          <div className='home-bar-left'>
           </div>
-          {dropdownBox}
-        </div>
-      </nav>
-    );
-  }
-
-  sessionLinks() {
-    return(
-      <nav className="home-bar">
-        <div onClick={this.props.demoLogin} className='home-demo'>
-          Demo Login
-        </div>
-
-        <div className='home-bar-right'>
-          <div className='home-login'><Link to="/login">Log In</Link></div>
-          <div className='home-signup'><Link to="/signup">Sign up</Link></div>
-        </div>
-
-      </nav>
-    );
-  }
-
-  render() {
-    if (this.props.currentUser) {
-      return this.personalGreeting();
+          <div className='home-bar-right'>
+            <div onClick={this.toggleDropdown} className='dropdown' >
+              <img className='home-avatar' src={currentUser.avatar_url} />
+              <img className='down-icon' src={window.staticImages.downIcon} />
+            </div>
+            {dropdownBox}
+          </div>
+        </nav>
+      );
     } else {
-      return this.sessionLinks();
+      return(
+        <nav className="home-bar">
+          <div onClick={this.props.demoLogin} className='home-demo'>
+            Demo Login
+          </div>
+          <div className='home-bar-right'>
+            <div className='home-login'><Link to="/login">Log In</Link></div>
+            <div className='home-signup'><Link to="/signup">Sign up</Link></div>
+          </div>
+        </nav>
+      );
     }
   }
 }
-
-export default HomeBar;
