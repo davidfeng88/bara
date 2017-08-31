b0ee128379b7e0cfe960aaaa16a77b6c98bac787
davidfeng88 <david.feng.ge@gmail.com>
2017-08-31 11:25:27 -0400

    organize stylesheet

--- frontend/components/search/filter_form.jsx
+++ frontend/components/search/filter_form.jsx
@@ -66,36 +66,3 @@ class FilterForm extends React.Component {
 }
 
 export default withRouter(FilterForm);
-// clean up css!
-// return(
-//   <div className="filter">
-//     <div className='price-filter'>
-//       <div className='price-filter-col'>
-//         <label htmlFor='min-price'>Minimum Price</label>
-//         <select id="min-price" value={minPrice}
-//           onChange={handleChange('minPrice', updateFilter)} >
-//           <option value='1' >$ - Inexpensive</option>
-//           <option value='2' >$$ - Moderate</option>
-//           <option value='3' >$$$ - Pricey</option>
-//           <option value='4' >$$$$ - Ultra High-End</option>
-//         </select>
-//       </div>
-//       <div className='price-filter-col'>
-//         <label htmlFor='max-price'>Maximum Price</label>
-//         <select id="max-price" value={maxPrice}
-//           onChange={handleChange('maxPrice', updateFilter)} >
-//           <option value='1' >$ - Inexpensive</option>
-//           <option value='2' >$$ - Moderate</option>
-//           <option value='3' >$$$ - Pricey</option>
-//           <option value='4' >$$$$ - Ultra High-End</option>
-//         </select>
-//       </div>
-//
-//     </div>
-//
-//     <div className='reset-filter'
-//       onClick={handleClick(resetFilter, updateFilter)}>
-//       Reset filters
-//     </div>
-//
-//   </div>
