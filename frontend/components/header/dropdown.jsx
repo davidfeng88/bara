077d2cfed0848e33a080efb3d4d874ab4907252a
davidfeng88 <david.feng.ge@gmail.com>
2017-09-01 01:16:08 -0400

    add little triangle to dropbox

--- frontend/components/header/dropdown.jsx
+++ frontend/components/header/dropdown.jsx
@@ -1,23 +1,37 @@
 import React from 'react';
 
-// stopPropagation: when you click on the dropdown box,
-// it should not toggle/close the dropdown
 const Dropdown = ({ currentUser, logout, toggleDropdown }) => (
-  <div onClick={toggleDropdown} className="dropdown-background">
-    <div className='dropdown-container'>
-      <div onClick={ e => e.stopPropagation() } className='dropdown-box'>
-        <div className='dropdown-box-row1' >
-          <div className='dropdown-avatar-wrapper'>
-            <img className='dropdown-avatar' src={currentUser.avatar_url} />
-          </div>
-          <div className='dropdown-name'>{currentUser.username}</div>
+  <div>
+    <div onClick={toggleDropdown} className="dropdown-background">
+    </div>
+    <div className='dropdown-box'>
+      <div className='dropdown-box-row1' >
+        <div className='dropdown-avatar-wrapper'>
+          <img className='dropdown-avatar' src={currentUser.avatar_url} />
         </div>
-        <div onClick={logout} className='dropdown-box-row2' >
-          Log Out
-        </div>
+        <div className='dropdown-name'>{currentUser.username}</div>
+      </div>
+      <div onClick={logout} className='dropdown-box-row2' >
+        Log Out
       </div>
     </div>
   </div>
 );
 
 export default Dropdown;
+//
+// <div onClick={toggleDropdown} className="dropdown-background">
+//   <div className='dropdown-container'>
+//     <div onClick={ e => e.stopPropagation() } className='dropdown-box'>
+//       <div className='dropdown-box-row1' >
+//         <div className='dropdown-avatar-wrapper'>
+//           <img className='dropdown-avatar' src={currentUser.avatar_url} />
+//         </div>
+//         <div className='dropdown-name'>{currentUser.username}</div>
+//       </div>
+//       <div onClick={logout} className='dropdown-box-row2' >
+//         Log Out
+//       </div>
+//     </div>
+//   </div>
+// </div>
