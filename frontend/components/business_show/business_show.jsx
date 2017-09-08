ad0ea565026ae8e292ad847912ccad4ea30620f1
davidfeng88 <david.feng.ge@gmail.com>
2017-09-07 20:10:59 -0400

    rewrite business show render logic

--- frontend/components/business_show/business_show.jsx
+++ frontend/components/business_show/business_show.jsx
@@ -15,9 +15,9 @@ export default class BusinessShow extends React.Component {
   constructor(props) {
     super(props);
     this.state = ({
-      loaded: false,
-      errors: [],
       business: null,
+      errors: [],
+      loaded: false,
     });
 
     this.clearErrors = this.clearErrors.bind(this);
@@ -30,15 +30,15 @@ export default class BusinessShow extends React.Component {
   componentDidMount() {
     fetchBusiness(this.props.match.params.id)
       .then(
-        (business) => this.setState({
-          loaded: true,
+        business => this.setState({
           business,
           errors: [],
-        }),
-        (errors) => this.setState({
           loaded: true,
-          errors: errors.responseJSON,
+        }),
+        errors => this.setState({
           business: null,
+          errors: errors.responseJSON,
+          loaded: true,
         })
       );
     window.scrollTo(0,0);
@@ -47,85 +47,74 @@ export default class BusinessShow extends React.Component {
   componentWillReceiveProps(nextProps) {
     if (nextProps.match.params.id !== this.props.match.params.id) {
       this.setState({
-        loaded: false,
         business: null,
         errors: [],
+        loaded: false,
       });
       fetchBusiness(nextProps.match.params.id)
         .then(
-          (business) => this.setState({
-            loaded: true,
+          business => this.setState({
             business,
             errors: [],
-          }),
-          (errors) => this.setState({
             loaded: true,
-            errors: errors.responseJSON,
+          }),
+          errors => this.setState({
             business: null,
+            errors: errors.responseJSON,
+            loaded: true,
           })
         );
       window.scrollTo(0,0);
     }
   }
 
-
   render() {
-    const { business } = this.state;
-    if (this.state.loaded) {
-      if (business) {
-        let reviewsEntries =
-          <div className='review-placehoder'>
-            No review for this business yet.
-          </div>;
-        return(
-          <div>
-            <div className='business-show-title'>
-              <div className='center'>
-                {businessShowTitle(business)}
-                <div className='business-show-title-row2'>
-                  <div className='info'>
-                    <ShowMap business={business} />
-                    {textInfo(business)}
-                  </div>
-                  <div className='pictures'>
-                    <img src={window.staticImages.businessDefault1} />
-                    <img src={business.image_url} />
-                    <img src={window.staticImages.businessDefault2} />
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <div className='review-index-main'>
-              <div className='center index-grid show-grid'>
-                <div className='index-grid-col1 show-grid-col1'>
-                  <h2>Reviews for
-                  <span className='normal black'> {business.name}</span></h2>
-                  <ReviewIndexContainer reviews={business.reviews} />
-                </div>
-                {extraInfo(business)}
-              </div>
-            </div>
-
-
-
-          </div>
-        );
-      } else {
-        return(
-          <div className='center'>
-            <ErrorList errors={ this.state.errors }
-              clearErrors={this.clearErrors} />
-            <Link to="/" className='link-as-button'>
-              Go Home
-            </Link>
-          </div>
-        );
-      }
-    } else {
+    const { business, errors, loaded } = this.state;
+    if (!loaded) {
       return(
         <img className='spinner' src={window.staticImages.spinner} />
       );
     }
+    if (!business) {
+      return(
+        <div className='center'>
+          <ErrorList errors={errors}
+            clearErrors={this.clearErrors} />
+          <Link to="/" className='link-as-button'>
+            Go Home
+          </Link>
+        </div>
+      );
+    }
+    return(
+      <div>
+        <div className='business-show-title'>
+          <div className='center'>
+            {businessShowTitle(business)}
+            <div className='business-show-title-row2'>
+              <div className='info'>
+                <ShowMap business={business} />
+                {textInfo(business)}
+              </div>
+              <div className='pictures'>
+                <img src={window.staticImages.businessDefault1} />
+                <img src={business.image_url} />
+                <img src={window.staticImages.businessDefault2} />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className='review-index-main'>
+          <div className='center index-grid show-grid'>
+            <div className='index-grid-col1 show-grid-col1'>
+              <h2>Reviews for
+              <span className='normal black'> {business.name}</span></h2>
+              <ReviewIndexContainer reviews={business.reviews} />
+            </div>
+            {extraInfo(business)}
+          </div>
+        </div>
+      </div>
+    );
   }
 }
