b31fed9982d6fe2aa280c56f352e2dc1cc3be95c
davidfeng88 <david.feng.ge@gmail.com>
2018-01-14 23:00:13 -0500

    make functions smaller

--- frontend/bara.jsx
+++ frontend/bara.jsx
@@ -1,21 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import Root from './components/root';
 import configureStore from './store/store';
 
-document.addEventListener( 'DOMContentLoaded', () => {
-  let store;
-  if ( window.currentUser ) {
-    const preloadedState = {
-      currentUser: window.currentUser
-    };
-    store = configureStore( preloadedState );
-    delete window.currentUser;
-  } else {
-    store = configureStore();
-  }
-
+const renderReactRoot = () => {
+  const store = configureStore();
   const root = document.getElementById( 'root' );
   ReactDOM.render( <Root store={store} />, root );
-} );
+};
+
+document.addEventListener( 'DOMContentLoaded', renderReactRoot );
--- frontend/store/store.js
+++ frontend/store/store.js
@@ -3,25 +3,50 @@ import {
   applyMiddleware
 } from 'redux';
 import thunk from 'redux-thunk';
-
 import RootReducer from '../reducers/root_reducer';
 
-const middlewares = [ thunk ];
+const configureMiddlewares = () => {
+  const middlewares = [ thunk ];
+  if ( process.env.NODE_ENV !== 'production' ) {
+    addReduxLoggerToMiddleware( middlewares );
+  }
+  return middlewares;
+};
 
-if ( process.env.NODE_ENV !== 'production' ) {
+const addReduxLoggerToMiddleware = ( middlewares ) => {
   // must use 'require' (import only allowed at top of file)
   const {
     createLogger
   } = require( 'redux-logger' );
   middlewares.push( createLogger() );
-}
+};
 
-const configureStore = ( preloadedState = {} ) => (
-  createStore(
-    RootReducer,
-    preloadedState,
-    applyMiddleware( ...middlewares )
-  )
-);
+const configurePreloadedState = () => {
+  let preloadedState = {};
+  if ( window.currentUser ) {
+    preloadedState = bootstrapCurrentUser();
+  }
+  return preloadedState;
+};
+
+const bootstrapCurrentUser = () => {
+  const preloadedState = {
+    currentUser: window.currentUser
+  };
+  delete window.currentUser;
+  return preloadedState;
+};
+
+const configureStore = () => {
+  const middlewares = configureMiddlewares();
+  const preloadedState = configurePreloadedState();
+  return (
+    createStore(
+      RootReducer,
+      preloadedState,
+      applyMiddleware( ...middlewares )
+    )
+  );
+};
 
 export default configureStore;
