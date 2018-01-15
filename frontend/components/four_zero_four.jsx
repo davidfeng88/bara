b400d3764379a1b5f676012a14c9c2583cccf902
davidfeng88 <david.feng.ge@gmail.com>
2018-01-15 15:04:57 -0500

    cleanup 404

--- frontend/components/four_zero_four.jsx
+++ frontend/components/four_zero_four.jsx
@@ -3,13 +3,21 @@ import {
   Link
 } from 'react-router-dom';
 
+const FourZeroFourMessage = () => (
+  <h1><strong>404 error.</strong>
+    &nbsp;The page you’re looking for cannot be found.</h1>
+);
+
+const GoHomeLink = () => (
+  <Link to="/" className='link-as-button'>
+    Go Home
+  </Link>
+);
+
 const FourZeroFour = () => (
   <div className='four-zero-four center'>
-    <h1><strong>404 error.</strong>
-      &nbsp;The page you’re looking for cannot be found.</h1>
-    <Link to="/" className='link-as-button'>
-      Go Home
-    </Link>
+    <FourZeroFourMessage />
+    <GoHomeLink />
   </div>
 );
 
