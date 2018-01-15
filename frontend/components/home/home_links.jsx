5ac556b87e4e4a9ccd1a8acbc5d6e29423e80e32
davidfeng88 <david.feng.ge@gmail.com>
2018-01-15 10:07:09 -0500

    cleanup

--- frontend/components/home/home_links.jsx
+++ frontend/components/home/home_links.jsx
@@ -3,28 +3,44 @@ import {
   Link
 } from 'react-router-dom';
 
+const homeRestaurantsLink = (
+  <span className='home-header-link'>
+    <i className="fa fa-cutlery" aria-hidden="true"></i>
+    <Link to="/businesses/?name=&location=New%20York">
+      Restaurants
+    </Link>
+  </span>
+);
+
+const homeNightlifeLink = (
+  <span className='home-header-link'>
+    <i className="fa fa-glass" aria-hidden="true"></i>
+    <Link to="/businesses/?name=&location=New%20York&tag=nightlife">
+      Nightlife
+    </Link>
+  </span>
+);
+
+const searchExample = (
+  <p>Search Example:&nbsp;
+    <Link to="/businesses/?name=burger&location=19th%20St">
+      find "burger" near "19th St"
+    </Link>
+  </p>
+);
+
+const logoInstruction = (
+  <p>
+    Click on bara logo for new random featured businesses
+  </p>
+);
+
 const HomeLinks = () => (
   <div className='home-header-links'>
-    <span className='home-header-link'>
-      <i className="fa fa-cutlery" aria-hidden="true"></i>
-      <Link to="/businesses/?name=&location=New%20York">
-        Restaurants
-      </Link>
-    </span>
-    <span className='home-header-link'>
-      <i className="fa fa-glass" aria-hidden="true"></i>
-      <Link to="/businesses/?name=&location=New%20York&tag=nightlife">
-        Nightlife
-      </Link>
-    </span>
-    <p>Search Example:&nbsp;
-      <Link to="/businesses/?name=burger&location=19th%20St">
-        find "burger" near "19th St"
-      </Link>
-    </p>
-    <p>
-      Click on bara logo for new random featured businesses
-    </p>
+    {homeRestaurantsLink}
+    {homeNightlifeLink}
+    {searchExample}
+    {logoInstruction}
   </div>
 );
 
