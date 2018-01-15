1d30f774d1924abea81c9633f92783206074b27d
davidfeng88 <david.feng.ge@gmail.com>
2018-01-15 17:18:38 -0500

    cleanup error list

--- frontend/components/error_list.jsx
+++ frontend/components/error_list.jsx
@@ -1,23 +1,37 @@
 import React from 'react';
 
-const errorItems = errors => (
-  errors.map(
-    ( error, idx ) => ( <li key={ `error-${idx}` }>{ error }</li> )
-  )
+const ErrorItems = ( {
+  errors
+} ) => (
+  <ul>
+    {errors.map(
+      ( error, idx ) => ( <li key={ `error-${idx}` }>{ error }</li> )
+    )}
+  </ul>
+);
+
+const DismissBotton = ( {
+  clearErrors
+} ) => (
+  <div onClick={clearErrors} className='dismiss-error'>
+  ×
+  </div>
 );
 
 const ErrorList = ( {
   errors,
   clearErrors
-} ) => (
-  errors.length === 0 ? null : (
-    <div className="error-list">
-      <ul>{errorItems(errors)}</ul>
-      <div onClick={clearErrors} className='dismiss-error'>
-      ×
+} ) => {
+  if ( errors.length === 0 ) {
+    return null;
+  } else {
+    return (
+      <div className="error-list">
+        <ErrorItems errors={errors} />
+        <DismissBotton clearErrors={clearErrors} />
       </div>
-    </div>
-  )
-);
+    );
+  }
+};
 
 export default ErrorList;
