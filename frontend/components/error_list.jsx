3500f8aabe82e59fbc63e91237c24c8ee9dd3ba7
davidfeng88 <david.feng.ge@gmail.com>
2018-02-04 22:46:56 -0500

    rewrite error list

--- frontend/components/error_list.jsx
+++ frontend/components/error_list.jsx
@@ -1,37 +1,54 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const ErrorItems = ( {
-  errors
-} ) => (
+const ErrorList = ({
+  errors,
+  clearErrors,
+}) => {
+  if (errors.length === 0) {
+    return null;
+  }
+  return (
+    <div className="error-list">
+      <ErrorItems errors={errors} />
+      <DismissBotton clearErrors={clearErrors} />
+    </div>
+  );
+};
+
+export default ErrorList;
+
+ErrorList.propTypes = {
+  errors: PropTypes.arrayOf(PropTypes.string).isRequired,
+  clearErrors: PropTypes.func.isRequired,
+};
+
+const ErrorItems = ({
+  errors,
+}) => (
   <ul>
-    {errors.map(
-      ( error, idx ) => ( <li key={ `error-${idx}` }>{ error }</li> )
-    )}
+    {errors.map(error => (<li key={error.length}>{error}</li>))}
   </ul>
 );
 
-const DismissBotton = ( {
-  clearErrors
-} ) => (
-  <div onClick={clearErrors} className='dismiss-error'>
+ErrorItems.propTypes = {
+  errors: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
+
+const DismissBotton = ({
+  clearErrors,
+}) => (
+  <div
+    onClick={clearErrors}
+    onKeyPress={clearErrors}
+    className="dismiss-error"
+    role="button"
+    tabIndex={0}
+  >
   ×
   </div>
 );
 
-const ErrorList = ( {
-  errors,
-  clearErrors
-} ) => {
-  if ( errors.length === 0 ) {
-    return null;
-  } else {
-    return (
-      <div className="error-list">
-        <ErrorItems errors={errors} />
-        <DismissBotton clearErrors={clearErrors} />
-      </div>
-    );
-  }
+DismissBotton.propTypes = {
+  clearErrors: PropTypes.func.isRequired,
 };
-
-export default ErrorList;
