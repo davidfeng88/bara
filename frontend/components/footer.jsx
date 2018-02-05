c8edfad296e7f53ad3f1515fb9e6279858647a59
davidfeng88 <david.feng.ge@gmail.com>
2018-02-04 22:36:10 -0500

    use template in footer

--- frontend/components/footer.jsx
+++ frontend/components/footer.jsx
@@ -1,10 +1,150 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
+const Footer = () => (
+  <div className="footer">
+    <div className="container footer-container">
+      <FooterLinks />
+      <FooterPicture />
+    </div>
+  </div>
+);
+
+export default Footer;
+
+const FooterLinks = () => (
+  <div className="footer-links">
+    <AboutBara />
+    <AboutMe />
+    <Credits />
+  </div>
+);
+
+const AboutBara = () => (
+  <FooterLinkColumnTemplate
+    title="About Bara"
+  >
+    <GitHubRepo />
+  </FooterLinkColumnTemplate>
+);
+
+const AboutMe = () => (
+  <FooterLinkColumnTemplate
+    title="About Me"
+  >
+    <PortfolioSite />
+    <GitHub />
+    <LinkedIn />
+    <AngelList />
+  </FooterLinkColumnTemplate>
+);
+
+const Credits = () => (
+  <FooterLinkColumnTemplate
+    title="Credits"
+  >
+    <DesignCredit />
+    <DeveloperCredit />
+    <IconsCredit />
+    <LogoDesignerCredit />
+  </FooterLinkColumnTemplate>
+);
+
+const GitHubRepo = () => (
+  <FooterLinkTemplate
+    url="https://github.com/davidfeng88/bara"
+    text="GitHub Repo"
+  />
+);
+
+const PortfolioSite = () => (
+  <FooterLinkTemplate
+    url="https://davidfeng.us/"
+    text="Portfolio Site"
+  />
+);
+
+const GitHub = () => (
+  <FooterLinkTemplate
+    url="https://github.com/davidfeng88"
+    text="GitHub"
+  />
+);
+
+const LinkedIn = () => (
+  <FooterLinkTemplate
+    url="https://www.linkedin.com/in/gfeng/"
+    text="LinkedIn"
+  />
+);
+
+const AngelList = () => (
+  <FooterLinkTemplate
+    url="https://angel.co/ge-david-feng"
+    text="AngelList"
+  />
+);
+
+const DesignCredit = () => (
+  <FooterLinkTemplate
+    title="Design: "
+    url="https://www.yelp.com/nyc"
+    text="Yelp"
+  />
+);
+
+const DeveloperCredit = () => (
+  <FooterLinkTemplate
+    title="Developer: "
+    url="https://davidfeng.us/"
+    text="Ge &#34;David&#34; Feng"
+  />
+);
+
+const IconsCredit = () => (
+  <FooterLinkTemplate
+    title="Icons: "
+    url="http://fontawesome.io/"
+    text="Font Awesome"
+  />
+);
+
+const LogoDesignerCredit = () => (
+  <FooterLinkTemplate
+    title="Logo Designer: "
+    url="https://www.linkedin.com/in/meng-zhang-692b7644/"
+    text="Meng Zhang"
+  />
+);
+
+const FooterPicture = () => (
+  <div className="footer-picture">
+    <img alt="" src={window.staticImages.footerPic} />
+  </div>
+);
+
+const FooterLinkColumnTemplate = ({
+  title,
+  children,
+}) => (
+  <div className="footer-links-col">
+    <h3>{title}</h3>
+    {children}
+  </div>
+);
+
+FooterLinkColumnTemplate.propTypes = {
+  title: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired,
+};
 
 const FooterLinkTemplate = ({
+  title,
   url,
   text,
 }) => (
   <p>
+    {title}
     <a
       href={url}
       target="_blank"
@@ -15,72 +155,14 @@ const FooterLinkTemplate = ({
   </p>
 );
 
+FooterLinkTemplate.propTypes = {
+  title: PropTypes.string,
+  url: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
+};
 
-
-const AboutBara = () => (
-  <div className="footer-links-col">
-    <h3>About Bara</h3>
-    <p>
-      <a
-        href="https://github.com/davidfeng88/bara"
-        target="_blank"
-        rel="noreferrer noopener"
-      >
-        GitHub Repo
-      </a>
-    </p>
-  </div>
-);
-
-const AboutMe = () => (
-  <div className="footer-links-col">
-    <h3>About Me</h3>
-      <p><a href="https://davidfeng.us/" target="_blank">
-      Portfolio Site</a></p>
-      <p><a href="https://github.com/davidfeng88" target="_blank">
-      GitHub</a></p>
-      <p><a href="https://www.linkedin.com/in/gfeng/" target="_blank">
-      LinkedIn</a></p>
-      <p><a href="https://angel.co/ge-david-feng" target="_blank">
-      AngelList</a></p>
-  </div>
-);
-
-const Credits = () => (
-  <div className="footer-links-col">
-    <h3>Credits</h3>
-      <p>Design: <a href="https://www.yelp.com/nyc" target="_blank">
-      Yelp</a></p>
-      <p>Developer: <a href="https://davidfeng.us/" target="_blank">
-      Ge "David" Feng</a></p>
-      <p>Icons: <a href="http://fontawesome.io/" target="_blank">
-      Font Awesome</a></p>
-      <p>Logo Designer: <a href="https://www.linkedin.com/in/meng-zhang-692b7644/" target="_blank">
-      Meng Zhang</a></p>
-  </div>
-);
-
-const FooterLinks = () => (
-  <div className="footer-links">
-    <AboutBara />
-    <AboutMe />
-    <Credits />
-  </div>
-);
-
-const FooterPicture = () => (
-  <div className='footer-picture'>
-    <img src={window.staticImages.footerPic} />
-  </div>
-);
-
-const Footer = () => (
-  <div className='footer'>
-    <div className='container footer-container'>
-      <FooterLinks />
-      <FooterPicture />
-    </div>
-  </div>
-);
-
-export default Footer;
+FooterLinkTemplate.defaultProps = {
+  title: '',
+  url: '',
+  text: '',
+};
