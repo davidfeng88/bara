f09d839d12858b9f90342b57cd1772f970d33cfc
davidfeng88 <david.feng.ge@gmail.com>
2020-05-18 21:21:03 -0400

    add basic user model tests

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -32,6 +32,6 @@ class Api::UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:username, :password, :avatar)
+    params.require(:user).permit(:username, :password)
   end
 end
---
+++ test/models/user_test.rb
@@ -0,0 +1,43 @@
+require 'test_helper'
+
+class UserTest < ActiveSupport::TestCase
+  def setup
+    @user = User.new(username: "Example User",
+                      password: "foobar")
+  end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
+
+  test "username should be present" do
+    @user.username = ""
+    assert_not @user.valid?
+  end
+
+  test "username should be unique" do
+    duplicate_user = @user.dup
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+
+  test "password should be present" do
+    @user.password = ""
+    assert_not @user.valid?
+  end
+
+  test "password should have a minimum length of 6" do
+    @user.password = "a" * 5
+    assert_not @user.valid?
+  end
+
+  # https://github.com/mhartl/sample_app_6th_ed/blob/master/test/models/user_test.rb
+  # test "associated reviews should be destroyed" do
+  #   @user.save
+  #   foo_biz = businesses(:foo_biz)
+  #   @user.reviews.create!(rating: 4, business_id: foo_biz.id)
+  #   assert_difference 'Review.count', -1 do
+  #     @user.destroy
+  #   end
+  # end
+end
